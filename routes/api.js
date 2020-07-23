8399182b1236bdfb7848a6f57245b60235175a5c
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2020-07-23 21:30:54 +0300

    Postman
    Testing get and post routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const MuseumUserTracker = require("../models/User");
 
-router.get("/museum-user-tracker", (req, res, next) => {});
+router.get("/museum-user-tracker", (req, res, next) => {
+  MuseumUserTracker.find({}, "action")
+    .then((data) => res.json(data))
+    .catch(next);
+});
 
-router.post("/museum-user-tracker", (req, res, next) => {});
+router.post("/museum-user-tracker", (req, res, next) => {
+  if (req.body.action) {
+    MuseumUserTracker.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: "The input field is empty",
+    });
+  }
+});
 
 router.get("/museum-user-tracker/log-in", (req, res, next) => {});
 
