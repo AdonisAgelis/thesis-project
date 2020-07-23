04ebf8eb16a8a109c4a7d80f90df9e7ade4dccdd
Fruit-Boy <stamkant96@gmail.com>
2020-07-23 20:24:22 +0300

    Set Up Express Backend / Routes

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/museum-user-tracker", (req, res, next) => {});
+
+router.post("/museum-user-tracker", (req, res, next) => {});
+
+router.delete("/museum-user-tracker/:id", (req, res, next) => {});
+
+module.exports = router;
