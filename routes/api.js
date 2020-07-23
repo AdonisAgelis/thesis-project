f9bcf48bd81114bf0212a6feb78da512ea57531e
Fruit-Boy <stamkant96@gmail.com>
2020-07-23 20:32:06 +0300

    Routes Part1

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,18 @@ router.get("/museum-user-tracker", (req, res, next) => {});
 
 router.post("/museum-user-tracker", (req, res, next) => {});
 
-router.delete("/museum-user-tracker/:id", (req, res, next) => {});
+router.get("/museum-user-tracker/log-in", (req, res, next) => {});
+
+router.post("/museum-user-tracker/log-in", (req, res, next) => {});
+
+router.get("/museum-user-tracker/sign-up", (req, res, next) => {});
+
+router.post("/museum-user-tracker/sign-up", (req, res, next) => {});
+
+router.get("/museum-user-tracker/admin", (req, res, next) => {});
+
+router.post("/museum-user-tracker/admin", (req, res, next) => {});
+
+router.get("/museum-user-tracker/info", (req, res, next) => {});
 
 module.exports = router;
