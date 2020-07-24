ee589dba55b64f27486c436bdfb94aed1327332a
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2020-07-24 15:49:47 +0300

    Front End config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import './custom.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css';
+
+import Main from './main.component';
+import SignUp from './signup.component';
+import LogIn from './login.component';
 
 class App extends Component {
   render() {
     return (
-      
+      <Router>
         <div className="container">
-          <h2>Museum User Tracker</h2>
+          <h2 className = "custom">Museum User Tracker</h2>
         </div>
-      
+        <Route path="/" exact component={Main} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={LogIn} />
+      </Router>
     );
   }
 }
