ee2f9ca7f11942bc4f8a4d81a2beb1ef19753ad9
Fruit-Boy <stamkant96@gmail.com>
2020-07-25 15:04:11 +0300

    V1 Update

---
+++ frontend/src/login.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function LogIn() {
+        return (
+            <div>
+                <p>Welcome to the LogIn page!</p>
+            </div>
+        )
+}
+
+export default LogIn;
