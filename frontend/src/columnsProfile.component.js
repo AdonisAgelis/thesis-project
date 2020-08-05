fc1840af400587c117f6602bf481e0450ee386af
Fruit-Boy <stamkant96@gmail.com>
2020-08-05 22:37:55 +0300

    Photo Background

--- frontend/src/columnsProfile.component.js
+++ frontend/src/columnsProfile.component.js
@@ -16,34 +16,33 @@ import Background from "./columnsback.jpg";
 import Buttons from "./buttons.component";
 import logo from "./logo.png";
 
-const ColumnsProfile = props => {
-
+const ColumnsProfile = (props) => {
   const [columnProp, setColumnProp] = useState(props);
 
   const sendColumnMidIsOpen = (childData) => {
     columnProp.parentCallback(childData);
   };
 
-    if (columnProp.columnPos === 2) {
-      return (
-        <MDBCol md={columnProp.columnPos}>
-          <MDBAnimation type="fadeInDown" delay=".3s">
-            <MDBCard style={{ opacity: "0.9" }}>
-              <MDBCardTitle
-                style={{
-                  display: "grid",
-                  placeItems: "center",
-                  textAlign: "center",
-                  marginTop: "4rem",
-                  color: "white",
-                  marginBottom: "5rem",
-                }}
-              >
-                <a href="http://localhost:3000">
-                  <img src={logo} className="img-fluid" />
-                </a>
+  if (columnProp.columnPos === 2) {
+    return (
+      <MDBCol md={columnProp.columnPos}>
+        <MDBAnimation type="fadeInDown" delay=".3s">
+          <MDBCard style={{ opacity: "0.9" }}>
+            <MDBCardTitle
+              style={{
+                display: "grid",
+                placeItems: "center",
+                textAlign: "center",
+                marginTop: "4rem",
+                color: "white",
+                marginBottom: "5rem",
+              }}
+            >
+              <a href="http://localhost:3000">
+                <img src={logo} className="img-fluid" />
+              </a>
 
-                {/* <h4>
+              {/* <h4>
                     <MDBIcon
                       icon="user"
                       style={{
@@ -54,94 +53,94 @@ const ColumnsProfile = props => {
                     />
                     Sensei
                   </h4> */}
+            </MDBCardTitle>
+            <hr
+              className="hr-light"
+              style={{ width: "90%", marginLeft: "5%" }}
+            />
+
+            <Buttons type="home" />
+
+            <Buttons type="new" func={sendColumnMidIsOpen} />
+            <Buttons type="load" />
+            <Buttons type="graph" />
+            <hr
+              className="hr-light"
+              style={{ width: "90%", marginLeft: "5%" }}
+            />
+            <Buttons type="reset" />
+            <Buttons type="logout" />
+          </MDBCard>
+        </MDBAnimation>
+      </MDBCol>
+    );
+  } else if (columnProp.columnPos === 7) {
+    return (
+      <MDBCol md={columnProp.columnPos}>
+        <MDBAnimation type="fadeInDown" delay=".4s">
+          <MDBCard style={{ background: "rgba(0, 0, 0, 0.9" }}>
+            <MDBCardImage className="img-fluid" />
+            <MDBCardBody>
+              <MDBCardTitle style={{ textAlign: "center" }}>
+                <h5 style={{ color: "white" }}>Name of Room</h5>
               </MDBCardTitle>
               <hr
                 className="hr-light"
                 style={{ width: "90%", marginLeft: "5%" }}
               />
 
-              <Buttons type="home" />
+              <MDBCardText style={{ textAlign: "center" }}></MDBCardText>
 
-              <Buttons type="new" func={sendColumnMidIsOpen} />
-              <Buttons type="load" />
-              <Buttons type="graph" />
+              <canvas id="myCanvas"></canvas>
               <hr
                 className="hr-light"
                 style={{ width: "90%", marginLeft: "5%" }}
               />
-              <Buttons type="reset" />
-              <Buttons type="logout" />
-            </MDBCard>
-          </MDBAnimation>
-        </MDBCol>
-      );
-    } else if (columnProp.columnPos === 7) {
-      return (
-        <MDBCol md={columnProp.columnPos}>
-          <MDBAnimation type="fadeInDown" delay=".4s">
-            <MDBCard style={{ background: "rgba(0, 0, 0, 0.9" }}>
-              <MDBCardImage className="img-fluid" />
-              <MDBCardBody>
-                <MDBCardTitle style={{ textAlign: "center" }}>
-                  <h5 style={{ color: "white" }}>Name of Room</h5>
-                </MDBCardTitle>
-                <hr
-                  className="hr-light"
-                  style={{ width: "90%", marginLeft: "5%" }}
-                />
+              <div
+                className="d-flex flex-row justify-content-around"
+                style={{ display: "flex", flexWrap: "wrap" }}
+              >
+                <Buttons type={"save"} />
+                <Buttons type={"group"} />
+                {/* <Buttons type={"people"} /> */}
+                <Buttons type={"add"} />
 
-                <MDBCardText style={{ textAlign: "center" }}></MDBCardText>
-
-                <canvas id="myCanvas"></canvas>
-                <hr
-                  className="hr-light"
-                  style={{ width: "90%", marginLeft: "5%" }}
-                />
-                <div
-                  className="d-flex flex-row justify-content-around"
-                  style={{ display: "flex", flexWrap: "wrap" }}
-                >
-                  <Buttons type={"save"} />
-                  <Buttons type={"group"} />
-                  {/* <Buttons type={"people"} /> */}
-                  <Buttons type={"add"} />
-
-                  <Buttons type={"run"} />
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBAnimation>
-        </MDBCol>
-      );
-    } else if (columnProp.columnPos === 3) {
-      return (
-        <MDBCol md={columnProp.columnPos}>
-          <MDBAnimation type="fadeInDown" delay=".5s">
-            <MDBCard style={{ background: "rgba(0, 0, 0, 0.9" }}>
-              <MDBCardImage className="img-fluid" />
-              <MDBCardBody>
-                <MDBCardTitle style={{ textAlign: "center" }}>
-                  <h5 style={{ color: "white", marginTop: "1rem" }}>
-                    <MDBIcon icon="tools" style={{ marginRight: "1rem" }} />
-                    Drag & Drop
-                  </h5>
-                  <hr
-                    className="hr-light"
-                    style={{ width: "90%", marginLeft: "5%" }}
-                  />
-                </MDBCardTitle>
-                <canvas id="myCanvas"></canvas>
-                <hr
-                  className="hr-light"
-                  style={{ width: "90%", marginLeft: "5%" }}
-                />
-                <MDBCardText style={{ textAlign: "center" }}></MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBAnimation>
-        </MDBCol>
-      );
-    }
-}
+                <Buttons type={"run"} />
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBAnimation>
+      </MDBCol>
+    );
+    // } else if (columnProp.columnPos === 3) {
+    //   return (
+    //     <MDBCol md={columnProp.columnPos}>
+    //       <MDBAnimation type="fadeInDown" delay=".5s">
+    //         <MDBCard style={{ background: "rgba(0, 0, 0, 0.9" }}>
+    //           <MDBCardImage className="img-fluid" />
+    //           <MDBCardBody>
+    //             <MDBCardTitle style={{ textAlign: "center" }}>
+    //               <h5 style={{ color: "white", marginTop: "1rem" }}>
+    //                 <MDBIcon icon="tools" style={{ marginRight: "1rem" }} />
+    //                 Drag & Drop
+    //               </h5>
+    //               <hr
+    //                 className="hr-light"
+    //                 style={{ width: "90%", marginLeft: "5%" }}
+    //               />
+    //             </MDBCardTitle>
+    //             <canvas id="myCanvas"></canvas>
+    //             <hr
+    //               className="hr-light"
+    //               style={{ width: "90%", marginLeft: "5%" }}
+    //             />
+    //             <MDBCardText style={{ textAlign: "center" }}></MDBCardText>
+    //           </MDBCardBody>
+    //         </MDBCard>
+    //       </MDBAnimation>
+    //     </MDBCol>
+    //   );
+  }
+};
 
 export default ColumnsProfile;
--- frontend/src/profile.component.js
+++ frontend/src/profile.component.js
@@ -4,42 +4,41 @@ import "./profile.css";
 import ColumnsProfile from "./columnsProfile.component";
 
 const Profile = () => {
-
-  const [midMenuisOpen, setMidMenuisOpen] = useState('1');
+  const [midMenuisOpen, setMidMenuisOpen] = useState("1");
 
   const callbackFunction = (childData) => {
     setMidMenuisOpen(childData);
   };
 
-    const columnPos = 2;
+  const columnPos = 2;
 
-    return (
-      <div id="profile">
-        <MDBView>
-          <MDBMask className="d-flex justify-content-center align-items-center gradient">
-            <ColumnsProfile
-              columnPos={columnPos}
-              parentCallback={callbackFunction}
-            />
-            {midMenuisOpen === "2" ? (
-              <ColumnsProfile columnPos={columnPos + 5} />
-            ) : (
-              true
-            )}
-            {midMenuisOpen === "2" ? (
+  return (
+    <div id="profile">
+      <MDBView>
+        <MDBMask className="d-flex justify-content-center align-items-center gradient">
+          <ColumnsProfile
+            columnPos={columnPos}
+            parentCallback={callbackFunction}
+          />
+          {midMenuisOpen === "2" ? (
+            <ColumnsProfile columnPos={columnPos + 5} />
+          ) : (
+            true
+          )}
+          {/* {midMenuisOpen === "2" ? (
               <ColumnsProfile columnPos={columnPos + 1} />
             ) : (
               true
-            )}
-            {midMenuisOpen === "3" ? (
-              <ColumnsProfile columnPos={columnPos + 5} />
-            ) : (
-              true
-            )}
-          </MDBMask>
-        </MDBView>
-      </div>
-    );
-}
+            )} */}
+          {midMenuisOpen === "3" ? (
+            <ColumnsProfile columnPos={columnPos + 5} />
+          ) : (
+            true
+          )}
+        </MDBMask>
+      </MDBView>
+    </div>
+  );
+};
 
 export default Profile;
