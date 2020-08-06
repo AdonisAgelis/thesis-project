c2fd49d7b7385040a1499793e0b26cb172fe98da
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2020-08-06 20:26:53 +0300

    SUFFERING FROM SUCCESS

--- frontend/src/room.component.js
+++ frontend/src/room.component.js
@@ -2,19 +2,35 @@ import React, { forwardRef, useState, useRef, useImperativeHandle } from 'react'
 import Square from './square.component';
 import DnDIcons from './dndIcons.component';
 
-const Room = (props) => {
+const renderSquare = (i, [itemX, itemY]) => {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    const isItemHere = x === itemX && y === itemY;
+    const piece = isItemHere ? <DnDIcons /> : null
+
+    return (
+        <div key = {i} style = {{width: '12.5%', height: '12.5%'}}>
+            <Square>{piece}</Square>
+        </div>
+    )
+}
+
+const Room = (props, { itemPosition }) => {
 
     const [roomProp, setRoomProp] = useState(props);
 
+    const squares = [];
+    for (let i = 0; i <64; i++) {
+        squares.push(renderSquare(i, roomProp.itemPosition));
+    }
+
     return (
-        <div 
-        style={{
-            width: '100%',
-            height: '100%'
-          }}>
-            {Square.renderSquare(0, 0, roomProp.itemPosition)}
-            {Square.renderSquare(1, 0, roomProp.itemPosition)}
-            {Square.renderSquare(2, 0, roomProp.itemPosition)}
+        <div
+            style={{
+                width: '100%',
+                height: '100%'
+            }}>
+            {roomProp.squares}
         </div>
     )
 }
--- frontend/src/square.component.js
+++ frontend/src/square.component.js
@@ -4,14 +4,6 @@ import DnDIcons from './dndIcons.component';
 const Square = (props) => {
 
     const [itemProp, setItemProp] = useState(props);
-    
-
-    const renderSquare = (x, y, [itemPosX, itemPosY]) => {
-        const isItemHere = itemPosX === x && itemPosY === y;
-        const piece = isItemHere ? <DnDIcons role = 'entrance' /> : null;
-
-        return <Square>{piece}</Square>
-    }
 
     const fill = 'black';
     return <div style={{ backgroundColor: fill,
