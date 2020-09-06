1e5dd387e6a180ad1129fb0dc2aa2e35e5541065
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2020-09-06 19:45:33 +0300

    GET Routes Working

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,15 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000;
 
+const indexRoute = require('./routes/index');
+
 mongoose.connect('mongodb://127.0.0.1:27017/MuseumUserTracker', { useNewUrlParser: true });
 const connection = mongoose.connection;
-
-const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/', router);
+app.use('/', indexRoute);
 
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
