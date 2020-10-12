5b6dee221442f5e4ef091e8710ac10d40bd5ae18
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2020-10-12 23:18:56 +0300

    Revert "Everything is updated correctly after resizing dimensions"
    This reverts commit 5b8fec8c89ba84a5a6b72c599541d62b45e61e68.

--- frontend/src/reducers/buttonEnabling.js
+++ frontend/src/reducers/buttonEnabling.js
@@ -5,10 +5,6 @@ const initialState = {
   
   const buttonEnablingReducer = (state = initialState, action) => {
     switch (action.type) {
-      case "RESIZED_DIMENSIONS":
-        state.disabledBtn = true;
-        state.disabledGroupBtns = true;
-        return state;
       case "ENABLE_BUTTONS_AFTER_CLICKING":
         state.disabledBtn = false;
         return state;
--- frontend/src/reducers/dropdownSecondSelection.js
+++ frontend/src/reducers/dropdownSecondSelection.js
@@ -3,9 +3,6 @@ const dropdownSecondSelectionsReducer = (
   action
 ) => {
   switch (action.type) {
-    case "DISABLE_ADD_BUTTON":
-      state = 'NUMBER OF PEOPLE';
-      return state;
     case "UPDATE_SECOND_DROPDOWN":
       return (state = action.payload);
     default:
