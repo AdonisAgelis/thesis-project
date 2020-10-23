ffbe106566dc0bbe3bf7e11dfdb943dd39680215
Fruit-Boy <stamkant96@gmail.com>
2020-10-23 18:40:15 +0300

    Room Model

---
+++ backend/models/room.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Room = mongoose.model(
+  "Room",
+  new mongoose.Schema({
+    name: String,
+    entrance: Number,
+    exit: Number,
+    accessPoint: [Number],
+    exhibit: [Number],
+    wall: [Number],
+    positionThatWillUndo: Number,
+    counterAccessPoint: Number,
+    counterExhibit: Number,
+    counterWall: Number,
+    counterAllPositions: Number,
+    allPositions: [Number],
+    entranceBadge: Number,
+    exitBadge: Number,
+    accessPointBadge: Number,
+    exhibitBadge: Number,
+    height: Number,
+    width: Number,
+    isResized: Boolean,
+    dropdownSelectionsReducer: Number,
+  })
+);
+
+module.exports = User;
