0aadbc18edc7027e904c69b4d27b85277d4a9bb5
Fruit-Boy <stamkant96@gmail.com>
2020-11-29 18:54:17 +0200

    Send to frontend array with rooms

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,42 +1,11 @@
 const { db } = require("../models/user.model");
 
 exports.allAccess = (req, res) => {
-  console.log(req.body.localStorageUserId.id);
-  // db.collection("rooms")
-  //   .find(userId)
-  //   .toArray()
-  //   .then((results) => {
-  //     // results = results.filter((userId) => userId == req.body.id);
-
-  //     //req.body.["id"] the id of user that is signed in!
-  //     //results comes from the db of "rooms"
-
-  //     console.log(req.body["id"]);
-
-  //     // const userIdValue = results[7]["userId"];
-  //     // useridd = { userId: userIdValue };
-  //     // if (req.body["id"] === useridd["userId"]) {
-  //     //   res.status(200).send(useridd);
-  //     // }
-  //     res.status(200).send(results);
-  //   })
-  //   .catch((error) => console.error.name(error));
   db.collection("rooms")
-    .find({ userId: "5fbc0ffa997c8524944cfefc" })
+    .find({ userId: req.body.localStorageUserId.id })
     .toArray()
     .then((results) => {
-      // results = results.filter((userId) => userId == req.body.id);
-
-      //req.body.["id"] the id of user that is signed in!
-      //results comes from the db of "rooms"
-
       console.log(results);
-
-      // const userIdValue = results[7]["userId"];
-      // useridd = { userId: userIdValue };
-      // if (req.body["id"] === useridd["userId"]) {
-      //   res.status(200).send(useridd);
-      // }
       res.status(200).send(results);
     })
     .catch((error) => console.error.name(error));
