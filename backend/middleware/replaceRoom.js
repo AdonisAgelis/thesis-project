9d74ca8be2ef36fea21656099820861d2d9adc11
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-08-24 22:04:13 +0300

    Replace Room feature #2

--- backend/middleware/replaceRoom.js
+++ backend/middleware/replaceRoom.js
@@ -1,16 +1,27 @@
-const { db } = require('../models');
+const db = require('../models');
 const Room = db.room;
 
 replaceRoomInDataBase = (req, res, next) => {
-  const deezRooms = db.collection('rooms').find({ roomId: req.body._id });
-
-  if (deezRooms) {
-    console.log('Rooms here');
-  } else {
-    console.log('No rooms here');
-  }
-
-  next();
+  // Room.findOne({ nameOfTemplate: req.body.nameOfTemplate }).exec(
+  //   (err, room) => {
+  //     if (err) {
+  //       console.log('Error my guy');
+  //       res.status(500).send({ message: err });
+  //       return;
+  //     }
+  //     if (room) {
+  //       console.log('This room already exists');
+  //       res.status(400).send({ message: 'This room already exists' });
+  //     }
+  //     next();
+  //   }
+  // );
+  // if (deezRooms) {
+  //   console.log('Rooms here');
+  // } else {
+  //   console.log('No rooms here');
+  // }
+  // next();
 };
 
 const replaceRoom = { replaceRoomInDataBase };
